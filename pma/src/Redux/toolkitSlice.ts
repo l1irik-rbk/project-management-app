fab281813ed2156160d206f791cabae861fba557
l1irik-rbk <kir.baranow2010@gmail.com>
2022-05-04 20:23:21 +0300

    feat: add default assembly

---
+++ pma/src/Redux/toolkitSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {},
+});
+
+export default appSlice.reducer;
