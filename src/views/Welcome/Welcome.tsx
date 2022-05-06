db859a4b4983c649aea2b719416f3368a0c81cac
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-05-06 18:40:34 +0300

    feat: add boar

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+
 import s from './Welcome.module.scss';
+import boar from '../../assets/images/boar.png';
 
 export const Welcome = () => {
-  return <div>Welcome</div>;
+  useEffect(() => {
+    document.title = 'Welcome';
+  }, []);
+
+  return (
+    <div className={s.content}>
+      <p>
+        Это приложение поможет организовать групповую работу в стиле канбан. Попробуй, ещё захочешь
+        <br />
+        <br />
+        Разработано командой из трёх мужиков на курсе React от rs.school
+      </p>
+      <img className={s.boar} src={boar} alt="" />
+      {/* <div className={s.boar}></div> */}
+    </div>
+  );
 };
