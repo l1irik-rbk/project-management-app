f131113f02572dc3ae26278daeb3c81138b187b9
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-05-18 11:42:55 +0300

    feat: add Spinner

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -4,6 +4,7 @@ import { findUser, getLogin } from '../../services/utils';
 import { User } from '../../services/interfaces/users';
 import { ProfileShow } from './ProfileShow/ProfileShow';
 import { ProfileEdit } from './ProfileEdit/ProfileEdit';
+import { Spinner } from '../../components/Spinner/Spinner';
 
 export const Profile = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -19,12 +20,15 @@ export const Profile = () => {
 
   return (
     <>
-      {user &&
-        (typeForm === 'show' ? (
+      {user ? (
+        typeForm === 'show' ? (
           <ProfileShow user={user} handleSubmit={() => setTypeForm('edit')} />
         ) : (
           <ProfileEdit user={user} onChangeTypeForm={() => setTypeForm('show')} />
-        ))}
+        )
+      ) : (
+        <Spinner />
+      )}
     </>
   );
 };
