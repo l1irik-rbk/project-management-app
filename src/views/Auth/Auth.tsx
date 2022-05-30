6e076f065dae06fcdc66ede265281e7efd1b5cbf
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-05-30 21:04:49 +0300

    refactor: change lines

--- src/views/Auth/Auth.tsx
+++ src/views/Auth/Auth.tsx
@@ -1,16 +1,17 @@
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import s from './Auth.module.scss';
 import { Toggle } from './Toggle/Toggle';
 import { LoginForm } from './LoginForm/LoginForm';
 import { SignupForm } from './SignupForm/SignupForm';
-import { useTranslation } from 'react-i18next';
 
 export const Auth = () => {
   const { t } = useTranslation();
 
   useEffect(() => {
     document.title = `${t('auth.docTitle')} | KanbanBoar`;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
